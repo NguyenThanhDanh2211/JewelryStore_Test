be37ead6e4945fcf404c321b10164472b48841f5
Nguyen Thanh Danh <nguyenthanhdanh221102@gmail.com>
2024-10-22 13:01:21 +0700

    Customer Registration Tests

--- cypress/e2e/tests/registerTest.cy.js
+++ cypress/e2e/tests/registerTest.cy.js
@@ -1,8 +1,8 @@
 import registerPage from '../../pages/registerPage';
 import registerData from '../../fixtures/authData.json';
 
-describe('Test Register Functionality', () => {
-  it('should allow user to register a new account', () => {
+describe('Customer Registration Tests', () => {
+  it('should allow registration with valid email and password', () => {
     registerPage.openHomePage();
 
     registerPage.clickAuthIcon();
@@ -16,5 +16,39 @@ describe('Test Register Functionality', () => {
     registerPage.clickRegisterButton();
 
     cy.contains('Registration successful!').should('be.visible');
+  });
+
+  it('should not allow registration with an email that already exists', () => {
+    registerPage.openHomePage();
+
+    registerPage.clickAuthIcon();
+
+    registerPage.clickRegisterLink();
+
+    registerPage.enterFullName(registerData.fullName);
+    registerPage.enterEmail(registerData.email);
+    registerPage.enterPassword(registerData.password);
+
+    registerPage.clickRegisterButton();
+
+    cy.contains('User with given email already exist!').should('be.visible');
+  });
+
+  it.only('should show an error message when password is less than 6 characters', () => {
+    registerPage.openHomePage();
+
+    registerPage.clickAuthIcon();
+
+    registerPage.clickRegisterLink();
+
+    registerPage.enterFullName(registerData.fullName);
+    registerPage.enterEmail(registerData.email);
+    registerPage.enterPassword(registerData.errorPassword);
+
+    registerPage.clickRegisterButton();
+
+    cy.contains('Password must be at least 6 characters long.').should(
+      'be.visible'
+    );
   });
 });
